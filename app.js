d70111cdaff24f4c69005bdbd4b386680df70bc3
Your Name <you@example.com>
2022-09-08 17:48:46 -0300

    adelanto del curso

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-let opcion = prompt(`
-Eliga una opcion:
-1:juegos
-2:series
-3:peliculas
-`);
+//let frutas = ["platano", "sandia", "perita", "uva"];
 
-console.log(opcion);
+//console.log(frutas.length);
+
+//for (let i = 0; i < frutas.length; i++) {
+//console.log(frutas[i]);
+//}
+
+//for (let fruta of frutas) {
+//console.log(fruta);
+//}
+
+//let nombre = "Ignacio";
+//let edad = 50;
+
+//for (let letra of nombre) {
+//}
+
+//function sumar(n1, n2) {
+//return parseInt(n1) + parseInt(n2);
+//}
+//let numero1 = prompt("Ingrese el primer numero");
+
+//let numero2 = prompt("Ingrese el segundo numero");
+
+//console.log(sumar(numero1, numero2));
+
+var nombreuser = "pepe";
+
+var nombreuser = "ramon";
+
+console.log(nombreuser);
