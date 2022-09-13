dbad03f0ea39e8592decc2230858a0f49887e1a7
Your Name <you@example.com>
2022-09-13 17:26:12 -0300

    practica de js

--- app.js
+++ app.js
@@ -1,32 +1,9 @@
-//let frutas = ["platano", "sandia", "perita", "uva"];
+const txtOp1 = document.getElementById("op1");
+const txtoperador = document.getElementById("operador");
+const txtOp2 = document.getElementById("op2");
+const btnCalcular = document.getElementById("calcular");
+const presultado = document.getElementById("resultado");
 
-//console.log(frutas.length);
-
-//for (let i = 0; i < frutas.length; i++) {
-//console.log(frutas[i]);
-//}
-
-//for (let fruta of frutas) {
-//console.log(fruta);
-//}
-
-//let nombre = "Ignacio";
-//let edad = 50;
-
-//for (let letra of nombre) {
-//}
-
-//function sumar(n1, n2) {
-//return parseInt(n1) + parseInt(n2);
-//}
-//let numero1 = prompt("Ingrese el primer numero");
-
-//let numero2 = prompt("Ingrese el segundo numero");
-
-//console.log(sumar(numero1, numero2));
-
-var nombreuser = "pepe";
-
-var nombreuser = "ramon";
-
-console.log(nombreuser);
+function calcular() {
+  console.log(txtoperador.value);
+}
