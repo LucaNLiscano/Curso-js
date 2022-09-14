6f07b2aba1c675861d326564ee1a2ba89308b326
Your Name <you@example.com>
2022-09-14 17:35:28 -0300

    cambio de curso a jonmircha

--- app.js
+++
@@ -1,9 +0,0 @@
-const txtOp1 = document.getElementById("op1");
-const txtoperador = document.getElementById("operador");
-const txtOp2 = document.getElementById("op2");
-const btnCalcular = document.getElementById("calcular");
-const presultado = document.getElementById("resultado");
-
-function calcular() {
-  console.log(txtoperador.value);
-}
