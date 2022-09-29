b9aaab63313ccf46a0888be9242298f06dc45b67
Your Name <you@example.com>
2022-09-29 17:36:35 -0300

    introduccion al DOM

--- Color picker/app.js
+++ Color picker/app.js
@@ -1,15 +1,17 @@
+//declaracion de las variables con el get by id.
 const inputColor = document.getElementById("inputColor");
 const btnVisualizar = document.getElementById("btnVisualizar");
 const parrafoExa = document.getElementById("parrafoExa");
 const cardColor = document.getElementById("cardColor");
 
+//prueba de confirmacion de las variables.
 //console.log(inputColor);
 //console.log(btnVisualizar);
 //console.log(parrafoExa);
 //console.log(cardColor);
 
+//al hacer click en el boton cambiara el texto al codigo del color y el color de abajo se cambia al color de arriba.
 btnVisualizar.addEventListener("click", () => {
-  console.log("confirmo");
   console.log(inputColor.value);
   parrafoExa.textContent = inputColor.value;
   cardColor.style.backgroundColor = inputColor.value;
